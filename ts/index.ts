3563d7efb8afeb9bc168e46ddcd6de7b5d877797
TheConnMan <bcconn2112@gmail.com>
2017-05-30 22:24:34 -0400

    Add FluentD logging

--- ts/index.ts
+++ ts/index.ts
@@ -5,6 +5,20 @@ import NpmService from './services/NpmService';
 
 var logger = log4js.getLogger();
 
+if (process.env.FLUENTD_HOST) {
+  var tags = (process.env.FLUENTD_TAGS ? process.env.FLUENTD_TAGS.split(',') : []).reduce((allTags, tag) => {
+    var pair = tag.split(':');
+    allTags[pair[0].trim()] = pair.length === 1 ? true : pair[1].trim();
+    return allTags;
+  }, {});
+  tags.function = 'npmjRSS';
+  log4js.addAppender(require('fluent-logger').support.log4jsAppender('npmjrss', {
+    host: process.env.FLUENTD_HOST,
+    timeout: 3.0,
+    tags: tags
+  }));
+}
+
 var app = express();
 
 app.use(express.static('public'));
@@ -13,11 +27,11 @@ var npmService = new NpmService();
 
 app.get('/package/:name', (req, res) => {
   npmService.getRss(req.params.name).then(rss => {
-    logger.info('Got feed for ' + req.params.name);
+    logger.info('Generated feed for ' + req.params.name);
     res.set('Content-Type', 'text/xml');
     res.send(rss);
   }).catch(e => {
-    logger.error('Unable to get RSS for ' + req.params.name, e);
+    logger.error('Unable to generate RSS feed for ' + req.params.name, e);
     res.sendStatus(500);
   });
 });
